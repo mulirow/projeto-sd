a8bde3d716600df3d0bb4b5d771408cd7b0e6f0a
Mulirow <mbn2@cin.ufpe.br>
2021-08-26 20:20:20 -0300

    Fixed 7-seg display

--- display.v
+++ display.v
@@ -1,95 +1,95 @@
-module display (data, disp1, disp0, enable);
-    
-    input [3:0] data;
-    input enable;
-    output reg [6:0] disp1;
-    output reg [6:0] disp0;
+module display (data, disp1, disp0);
+    input [4:0] data;
+    output [6:0] disp1;
+    output [6:0] disp0;
 
-    assign zero = 7'b0000001;  // "0"
-    assign one = 7'b1001111;  // "1"
-    assign two = 7'b0010010; // "2"
-    assign three = 7'b0000110;  // "3"
-    assign four = 7'b1001100;  // "4"
-    assign five = 7'b0100100; // "5"
-    assign six = 7'b0100000; // "6"
-    assign seven = 7'b0001111; // "7"
-    assign eight= 7'b0000000; // "8"
-    assign nine= 7'b0000100; // "9"
-    assign clr = 7'b1111111; // Clean display
+    reg disp0reg;
+    reg disp1reg;
+    reg zero = 7'b0000001;  // "0"
+    reg one = 7'b1001111;  // "1"
+    reg two = 7'b0010010; // "2"
+    reg three = 7'b0000110;  // "3"
+    reg four = 7'b1001100;  // "4"
+    reg five = 7'b0100100; // "5"
+    reg six = 7'b0100000; // "6"
+    reg seven = 7'b0001111; // "7"
+    reg eight= 7'b0000000; // "8"
+    reg nine= 7'b0000100; // "9"
+    reg clr = 7'b1111111; // Clean display
 
-    always @ (*)
-        begin
-            if(enable == 1'b0) begin
-                 case(data)
-                    4'b0000: begin
-                        disp1 = clr;
-                        disp0 = zero;
-                    end
-                    4'b0001: begin
-                        disp1 = clr;
-                        disp0 = one;
-                    end
-                    4'b0010: begin
-                        disp1 = clr;
-                        disp0 = two;
-                    end
-                    4'b0011: begin
-                        disp1 = clr;
-                        disp0 = three;
-                    end
-                    4'b0100: begin
-                        disp1 = clr;
-                        disp0 = four;
-                    end
-                    4'b0101: begin
-                        disp1 = clr;
-                        disp0 = five;
-                    end
-                    4'b0110: begin
-                        disp1 = clr;
-                        disp0 = six;
-                    end
-                    4'b0111: begin
-                        disp1 = clr;
-                        disp0 = seven;
-                    end
-                    4'b1000: begin
-                        disp1 = clr;
-                        disp0 = eight;
-                    end
-                    4'b1001: begin
-                        disp1 = clr;
-                        disp0 = nine;
-                    end
-                    4'b1010: begin
-                        disp1 = one;
-                        disp0 = zero;
-                    end
-                    4'b1011: begin
-                        disp1 = one;
-                        disp0 = one;
-                    end
-                    4'b1100: begin
-                        disp1 = one;
-                        disp0 = two;
-                    end
-                    4'b1101: begin
-                        disp1 = one;
-                        disp0 = three;
-                    end
-                    4'b1110: begin
-                        disp1 = one;
-                        disp0 = four;
-                    end
-                    4'b1111: begin
-                        disp1 = one;
-                        disp0 = five;
-                    end
-                endcase
-            end else begin
-                disp1 = clr;
-                disp0 = clr;
+    always @ (*) begin
+        case(data)
+            5'b00000: begin
+                disp1reg <= clr;
+                disp0reg <= zero;
             end
-        end
+            5'b00001: begin
+                disp1reg <= clr;
+                disp0reg <= one;
+            end
+            5'b00010: begin
+                disp1reg <= clr;
+                disp0reg <= two;
+            end
+            5'b00011: begin
+                disp1reg <= clr;
+                disp0reg <= three;
+            end
+            5'b00100: begin
+                disp1reg <= clr;
+                disp0reg <= four;
+            end
+            5'b00101: begin
+                disp1reg <= clr;
+                disp0reg <= five;
+            end
+            5'b00110: begin
+                disp1reg <= clr;
+                disp0reg <= six;
+            end
+            5'b00111: begin
+                disp1reg <= clr;
+                disp0reg <= seven;
+            end
+            5'b01000: begin
+                disp1reg <= clr;
+                disp0reg <= eight;
+            end
+            5'b01001: begin
+                disp1reg <= clr;
+                disp0reg <= nine;
+            end
+            5'b01010: begin
+                disp1reg <= one;
+                disp0reg <= zero;
+            end
+            5'b01011: begin
+                disp1reg <= one;
+                disp0reg <= one;
+            end
+            5'b01100: begin
+                disp1reg <= one;
+                disp0reg <= two;
+            end
+            5'b01101: begin
+                disp1reg <= one;
+                disp0reg <= three;
+            end
+            5'b01110: begin
+                disp1reg <= one;
+                disp0reg <= four;
+            end
+            5'b01111: begin
+                disp1reg <= one;
+                disp0reg <= five;
+            end
+            default: begin
+                disp1reg <= clr;
+                disp0reg <= clr;
+            end
+        endcase
+    end
 
+    assign disp0 = disp0reg;
+    assign disp1 = disp1reg;
 endmodule
